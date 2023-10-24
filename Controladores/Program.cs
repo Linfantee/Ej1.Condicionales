53ffef8a3494ae486897cea1367b9234cafbae54
Linfantee <145344308+Linfantee@users.noreply.github.com>
2023-10-24 12:25:14 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,33 @@
+﻿using Ej1.Condicionales.Servicios;
+
+namespace Ej1.Condicionales
+{
+
+    class Program
+    {
+
+        static void Main(string[] args) {
+
+  
+          
+
+            PeticionInterfaz ao = new PeticionImplementacion();
+            int NumeroRecogido;
+            NumeroRecogido = ao.PeticionNumero();
+            
+            if ((NumeroRecogido % 2)== 0 ) 
+            {
+                Console.WriteLine("Número par");
+            }
+            else 
+            {
+                Console.WriteLine("Número impar");
+            }
+        
+        }
+
+
+    }
+
+}
+
---
+++ Servicios/PeticionImplementacion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej1.Condicionales.Servicios
+{
+    internal class PeticionImplementacion : PeticionInterfaz
+    {
+        public int PeticionNumero()
+        {
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Introduzca número: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+
+
+            return n1;
+        }
+    }
+}
